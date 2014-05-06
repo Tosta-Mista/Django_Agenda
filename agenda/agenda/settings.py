ffcd9edacda9dad92abc22bdb31b7d750a5491cc
José Gonçalves <jose.goncalves@dlcproduction.ch>
2014-05-06 19:21:11 +0200

    add comment about template dir.

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
+    'personal_calendar',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,6 +83,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Template directory : related path
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
