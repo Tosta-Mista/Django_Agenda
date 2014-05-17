5ac7a87a236782b0690246fd1017c9dc5b761c5c
José Gonçalves <jose.goncalves@dlcproduction.ch>
2014-05-17 23:29:33 +0200

    protect accounts profile with the decorator login_required

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -1,6 +1,7 @@
 # -*- coding : utf-8 -*-
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
@@ -13,6 +14,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', login),
     url(r'^accounts/logout/$', logout, {'next_page':'/accounts/login/'}),
-    url(r'^accounts/profile/$', TemplateView.as_view(template_name="registration/profile.html")),
+    url(r'^accounts/profile/$', login_required(TemplateView.as_view(template_name="registration/profile.html"))),
 
 )
