d963590804fbde6fcd3a0e8b676729f94c93b50c
José Gonçalves <jose.goncalves@dlcproduction.ch>
2014-05-18 03:46:38 +0200

    change a lot of things... use generic views instead of views

--- agenda/personal_calendar/urls.py
+++ agenda/personal_calendar/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.list import ListView
 from models import Event
 
 urlpatterns = patterns('',
-    url(r'^lists/$', ListView.as_view(model= Event)),
+    url(r'^lists/$', ListView.as_view(model= Event, paginate_by=10), name='agenda_list'),
     url(r'^create/$', create),
     url(r'^(\+d)/details/$', details),
 )
